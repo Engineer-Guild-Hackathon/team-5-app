d32a4203e9e5ba35bcc28c9b91ef977b92c2b875
taragodmentai <kazuto727e.x@gmail.com>
2025-09-19 03:52:29 +0900

    add DB module

--- backend/app/db/models.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import Field, SQLModel
-
-class User(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    username: str = Field(index=True, unique=True)
-    email: str = Field(unique=True)
-    hashed_password: str # 重要：生のパスワードではなくハッシュ化された値を保存
