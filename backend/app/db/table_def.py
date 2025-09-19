4484f3e7292a70c7a8af9ecd83b086963d6baac9
taragodmentai <kazuto727e.x@gmail.com>
2025-09-19 15:31:32 +0900

    fix 忘れ

--- backend/app/db/table_def.py
+++ backend/app/db/table_def.py
@@ -4,9 +4,6 @@ class User_table(SQLModel, table=True):
     Email: str = Field(nullable=False,unique=True)
     UserName: str = Field(nullable=False)
     Password: str = Field(nullable=False)
-<<<<<<< HEAD
-    Id: str = Field(nullable=False,unique=True,primary_key=True)
-=======
     Id: str = Field(nullable=False,unique=True,primary_key=True)
 
 class User_log(SQLModel,table=True):
@@ -18,4 +15,3 @@ class User_score_log(SQLModel,table=True):
     Id:str = Field(nullable=False,unique=True,primary_key=True,foreign_key="user_table.id")
     original:str = Field(nullable=False)
     score:int = Field(nullable=False)
->>>>>>> fc1a10f5a5adc39db13bd0375053eaf46a79533b
