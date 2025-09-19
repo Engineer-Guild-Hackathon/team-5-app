edb58cd3948081cb5503a8bcecde9de9e70a2994
imkimkim <imkimkim07@gmail.com>
2025-09-19 11:49:18 +0000

    db

--- backend/app/db/table_def.py
+++ backend/app/db/table_def.py
@@ -1,4 +1,7 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Column
+from typing import Dict, Any,Optional
+from sqlalchemy.dialects.mysql import JSON
+
 
 class User_table(SQLModel, table=True):
     Email: str = Field(nullable=False,unique=True)
@@ -6,10 +9,13 @@ class User_table(SQLModel, table=True):
     Password: str = Field(nullable=False)
     Id: str = Field(nullable=False,unique=True,primary_key=True)
 
-class User_log(SQLModel,table=True):
-    Id:str = Field(nullable=False,unique=True,primary_key=True)
-    original:str = Field(nullable=False)
-    translated:str = Field(nullable=False)
+class User_log(SQLModel, table=True):
+    log_id: Optional[int] = Field(default=None, primary_key=True)
+    Id: str = Field(foreign_key="user_table.Id")
+    translated_sentence: str = Field(sa_column=Column(JSON))
+    base_language: str = Field(nullable=False)
+    translated_language: str = Field(nullable=False)
+
 
 class User_score_log(SQLModel,table=True):
     Id:str = Field(nullable=False,unique=True,primary_key=True,foreign_key="user_table.Id")
